93810e7ef7f0219c3ac34a036de3c488fc0940c1
Jorge Sanchez <siqueiros0702@gmail.com>
2022-05-29 18:48:52 -0500

    project

---
+++ Ingredientes_list.java
@@ -0,0 +1,30 @@
+/**
+ * Write a description of class Ingredientes_list here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ingredientes_list  
+{
+    // instance variables - replace the example below with your own
+    private int x; 
+
+    /**
+     * Constructor for objects of class Ingredientes_list
+     */
+    public Ingredientes_list()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
